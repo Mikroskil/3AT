7d21d4595972499dc6ad6bd932f2a54c77db77ca
eternalBlast <lunar_dr1ve@yahoo.com>
2014-01-13 21:23:56 +0700

    tambah file gambar

--- register.php
+++ register.php
@@ -1,1 +1,24 @@
-regis 
+<?php
+session_start();
+if(!empty($_SESSION['namamember'])){
+?>
+	<script type="text/javascript">
+		alert("Anda sudah Log In...!!! Tidak berhak mendaftar kembali...!!");
+	</script>
+<?php
+	echo "<meta http-equiv='refresh' content='0; url=index.php'>";
+}
+else{
+include('koneksi.php');
+$nama=$_POST['nama'];
+$user=$_POST['user'];
+$pass=md5($_POST['pass']);
+mysql_query("insert into tbl_member (nama,user,pass) values ('$nama','$user','$pass')");
+?>
+	<script type="text/javascript">
+		alert("Anda sudah terdaftar..!!! Sekarang anda bisa Log In...!!");
+	</script>
+<?php
+	echo "<meta http-equiv='refresh' content='0; url=index.php'>";
+}
+?>
